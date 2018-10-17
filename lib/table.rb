f6295edd8bd6a5610c608b7754bb33d394ac8045
anita nseri <anaseri@student.unimelb.edu.au>
2018-10-17 12:34:59 +1100

    checking the validity of given position

--- lib/table.rb
+++ lib/table.rb
@@ -4,4 +4,13 @@ class Table
     @length = length
     @width = width
   end
+
+  # coordinates x and y can't be negative, and can't be larger than table size
+  def position_is_valid?(position)
+    position.x < @length &&
+    position.y < @width &&
+    position.x >= 0 &&
+    position.y >= 0
+  end
+  
 end
