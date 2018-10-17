303770ef0fae0e73cc665470ebe051c29b65bbb2
anita nseri <anaseri@student.unimelb.edu.au>
2018-10-17 14:10:30 +1100

    movements and directions

--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -32,4 +32,96 @@ describe Robot do
     end
   end
 
+  describe '#report' do
+    let(:position) { Position.new(2, 3, 'NORTH') }
+
+    context 'after inicial placement of the robot' do
+      it 'reports current position correctly' do
+        PlaceCommand.new(robot, table, position).execute
+
+        expect(robot.report).to eq('2,3,NORTH')
+      end
+    end
+
+    context 'before initial placement of the robot' do
+      it 'reports error message' do
+        expect(robot.report).to eq('not in place')
+      end
+    end
+  end
+
+  describe '#report' do
+    it 'returns a short report' do
+      position = Position.new(1, 1, 'NORTH')
+
+      PlaceCommand.new(robot, table, position).execute
+
+      expect(robot.report).to eq('1,1,NORTH')
+    end
+  end
+
+  describe '#execute_command' do
+    context 'when placed correctly' do
+      it 'correctly turns to the left' do
+        position = Position.new(1, 1, 'NORTH')
+
+        PlaceCommand.new(robot, table, position).execute
+
+        LeftCommand.new(robot).execute
+
+        expect(robot.report).to eq('1,1,WEST')
+      end
+
+      it 'correctly turns to the right' do
+        position = Position.new(1, 1, 'NORTH')
+
+        PlaceCommand.new(robot, table, position).execute
+
+        RightCommand.new(robot).execute
+
+        expect(robot.report).to eq('1,1,EAST')
+      end
+
+      it 'correctly moves NORTH' do
+        position = Position.new(1, 1, 'NORTH')
+
+        PlaceCommand.new(robot, table, position).execute
+
+        MoveCommand.new(robot, table).execute
+
+        expect(robot.report).to eq('1,2,NORTH')
+      end
+
+      it 'correctly moves SOUTH' do
+        position = Position.new(1, 1, 'SOUTH')
+
+        PlaceCommand.new(robot, table, position).execute
+
+        MoveCommand.new(robot, table).execute
+
+        expect(robot.report).to eq('1,0,SOUTH')
+      end
+
+      it 'correctly moves EAST' do
+        position = Position.new(1, 1, 'EAST')
+
+        PlaceCommand.new(robot, table, position).execute
+
+        MoveCommand.new(robot, table).execute
+
+        expect(robot.report).to eq('2,1,EAST')
+      end
+
+      it 'correctly moves WEST' do
+        position = Position.new(1, 1, 'WEST')
+
+        PlaceCommand.new(robot, table, position).execute
+
+        MoveCommand.new(robot, table).execute
+
+        expect(robot.report).to eq('0,1,WEST')
+      end
+    end
+  end
+
 end
