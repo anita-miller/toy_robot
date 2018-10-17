114d49b81921ae9baba3ed3aa24a44c2fcb1adea
anita nseri <anaseri@student.unimelb.edu.au>
2018-10-17 13:45:51 +1100

    checking valid,invalid pos

--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -8,4 +8,28 @@ require 'move_command'
 
 describe Robot do
 
+  let(:robot) { Robot.new }
+  let(:table) { Table.new(5, 5) }
+
+  describe '#place' do
+    before do
+      PlaceCommand.new(robot, table, position).execute
+    end
+
+    context 'valid position' do
+      let(:position) { Position.new(2, 3, 'NORTH') }
+
+      it 'robot is in the correct position' do
+        expect(robot.current_position).to eq(position)
+      end
+    end
+    context 'with invalid position' do
+      let(:position) { Position.new(9, 9, 'NORTH') }
+
+      it 'will not place robot' do
+        expect(robot.current_position).to be nil
+      end
+    end
+  end
+
 end
